e400e5ad368aae7863d6a2eefc906f43f991fa11
Angel Martin Olivera <Angel.MartinOlivera@chenmed.com>
2019-04-11 11:12:37 -0400

    build js file

---
+++ nggraqhql/build/nggraphql.js
@@ -0,0 +1,48 @@
+(function() {
+    'use strict';
+    angular
+        .module('ngGraphqlConfig', [])
+        .value('ngGraphqlConfig',{
+            url:'',
+            asJSON:true,
+            headers:{}
+        });
+
+    angular
+        .module('ngGraphql', ['ngGraphqlConfig']);
+
+    // Common.js package manager support (e.g. ComponentJS, WebPack)
+    if (typeof module !== 'undefined' && typeof exports !== 'undefined' && module.exports === exports) {
+        module.exports = 'ngGraphql';
+    }
+})();
+(function () {
+    'use strict';
+
+    angular
+        .module('ngGraphql')
+        .factory('ngGraphqlService', ngGraphqlService);
+
+        /* @ngInject */
+        function ngGraphqlService($rootScope,ngGraphqlConfig){
+            var graphQLConnetion = graphql(ngGraphqlConfig.url, {
+                asJSON: ngGraphqlConfig.asJSON,
+                headers: ngGraphqlConfig.headers
+            });
+
+            return {
+                executeQuery: executeQuery
+            }
+
+            function executeQuery(query,params){
+                var client = graphQLConnetion(query);
+                $rootScope.$broadcast('ngGraphql:executing');
+                return client(params)
+                .then(function (response) {
+                    $rootScope.$broadcast('ngGraphql:finished');
+                    return response;
+                  });
+            }
+        }
+
+})();
